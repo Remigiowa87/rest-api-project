01900263de6c1ed66049f4a45a015de5d8bcf650
Remigio <remigiowa87@gmail.com>
2024-07-23 13:45:07 +0200

    added getenv

--- app.py
+++ app.py
@@ -37,9 +37,8 @@ def create_app(db_url=None):
 
     api = Api(app)
 
-    # app.config["JWT_SECRET_KEY"] = secrets.SystemRandom().getrandbits(128)
-    # app.config["JWT_SECRET_KEY"] = "113672007235347011625727468453977326023"
-    app.config["JWT_SECRET_KEY"] = "jose"
+ 
+    app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
     jwt = JWTManager(app)
 
 
